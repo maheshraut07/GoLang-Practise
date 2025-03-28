8249242ed7317c804d2e924709f2044c14f46eba
maheshraut07 <rautmahesh213@gmail.com>
2025-03-28 18:51:16 +0530

    modification in some of the files

---
+++ Codes/00sample/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "mahesh";
+    fmt.Println("the username is : ", username)
+	fmt.Printf("the type of username is : %T \n",username)
+}
