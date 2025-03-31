508f9ff7d9bb0985ed6355d2872d2712255e6124
maheshraut07 <rautmahesh213@gmail.com>
2025-03-31 13:07:39 +0530

    modify the codes in the 00sample folder

--- Codes/00sample/main.go
+++ Codes/00sample/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
 
 func main() {
-	var username string = "mahesh";
-    fmt.Println("the username is : ", username)
-	fmt.Printf("the type of username is : %T \n",username)
+    
+    
+    var subjects = []string{"maths", "english", "science"}
+    var names = [3]string{"mahesh", "sumit", "vicky"}
+
+    fmt.Printf("The type of the names array is: %T\n", names)
+    fmt.Println()
+    fmt.Printf("The type of the subject slice is: %T\n", subjects)
+    fmt.Println()
+    fmt.Println("This is slice:", subjects)
+
+    fmt.Println("The length of the subjects is:", len(subjects))
+    fmt.Println("The first two elements in the subjects are:", subjects[0:2])
+    fmt.Println()
+    
+    
+	reader := bufio.NewReader(os.Stdin)
+    fmt.Println("enter the any number: ")
+    input, _ := reader.ReadString('\n')
+    fmt.Println("the inputted number is : ", input)
 }
